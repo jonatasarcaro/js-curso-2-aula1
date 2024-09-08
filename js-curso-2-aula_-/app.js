4b5e5d455000a182ba5bbb74223ed7662723c6f5
jonatasarcaro <jontasarcaro943@gmail.com>
2024-09-08 14:05:21 -0300

    funcionando

--- js-curso-2-aula_-/app.js
+++ js-curso-2-aula_-/app.js
@@ -1,9 +1,62 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do número secreto';
+let numeroSecreto = gerarNumeraAleatorio();
+let tentativas = 1;
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 1 a 10';
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+function exibirMensagemInicial(){
+    exibirTextoNaTela('h1', 'Jogo do numero secreto');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+}
+
+exibirMensagemInicial();
+
+exibirTextoNaTela('h1', 'Jogo do numero secreto');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 
 function verificarChute() { // Função puxada do botão do index.html
-    console.log('O botão foi clicado')
+    let chute = document.querySelector('input').value;
+    
+    if (chute == numeroSecreto){
+        exibirTextoNaTela('h1', 'Acertou!');
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `você descobriu o número secreto com ${tentativas} ${palavraTentativa}`;
+        exibirTextoNaTela ('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela('p', 'O número secreto é menor');
+        } else {
+            exibirTextoNaTela('p', 'O número secreto é maior ');
+        } 
+        tentativas++;
+        LimparCampo();
+    }
 }
+
+function gerarNumeraAleatorio() {
+    return parseInt(Math.random() * 10 + 1);
+}
+
+function LimparCampo() {
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+function reiniciarJogo() {
+    numeroSecreto = gerarNumeraAleatorio();
+    LimparCampo();
+    tentativas = 1;
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', true);
+}
+
+
+
+
+
+
+
+
